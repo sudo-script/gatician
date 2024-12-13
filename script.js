e0d31f6909a03116004878b7b9b0406b6eda0c98
Anubhav Chauhan <87933819+sudo-script@users.noreply.github.com>
2024-12-13 20:13:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// Typewriter effect
+const textArray = ["Tailored for Modern Businesses.", "Manage Operations Efficiently.", "Ehance Your Productivity!"];
+let currentIndex = 0;
+let charIndex = 0;
+const typingSpeed = 100;
+const typewriterElement = document.getElementById("typewriter");
+
+function type() {
+  if (charIndex < textArray[currentIndex].length) {
+    typewriterElement.textContent += textArray[currentIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingSpeed);
+  } else {
+    setTimeout(erase, 2000);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typewriterElement.textContent = textArray[currentIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, typingSpeed / 2);
+  } else {
+    currentIndex = (currentIndex + 1) % textArray.length;
+    setTimeout(type, typingSpeed);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  type();
+});
